95b3f222570a64e48e930f261c8265e5fd516acc
guilhermeSmartins <mineg4500@gmail.com>
2021-06-09 10:07:18 -0300

    one more video added to wednesday

--- src/Modules/Wednesday.js
+++ src/Modules/Wednesday.js
@@ -2,11 +2,13 @@ class Wednesday {
     media;
     media_data = {
         0: 'https://i.imgur.com/iefnQbl.mp4',
-        1: 'https://i.imgur.com/myJX5iV.jpg'
+        1: 'https://i.imgur.com/myJX5iV.jpg',
+        2: 'https://i.imgur.com/9TK3EyW.mp4'
     }
 
     constructor() {
-        const whichMedia = Math.round(Math.random())
+        const whichMedia = Math.round(Math.random() * 3)
+        
         this.media = this.media_data[whichMedia]
     }
 
